576396ef25a195fd20b77785e0555bb4d9797260
Gilang R. Aprianto <gilangr.aprianto@gmail.com>
2025-07-04 04:22:47 +0700

    update

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'GIZIPP';
+export const SITE_DESCRIPTION = 'Sumber inspirasi dan panduan praktis untuk membangun produktivitas, memetakan potensi, dan menjaga pemulihan diri. Temukan “gizi” terbaik bagi pikiran dan keseharian Anda.';
